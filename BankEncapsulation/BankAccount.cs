2865fe3f256a48181927dc68ccea43adf3def832
Lisa Walters <lisacampiwalters@gmail.com>
2023-03-21 20:11:44 -0600

    wrote Deposit and GetBalance methods

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance;
+        public void Deposit(double depositAmount)
+        {
+            balance += depositAmount;
+        }
+
+        public double GetBalance()
+        { 
+            return balance;
+        }
+    }//end BankAccount class
+}
